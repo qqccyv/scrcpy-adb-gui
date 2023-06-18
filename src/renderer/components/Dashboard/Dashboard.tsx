778779337f25f2ab5375e0bcc10a527367f07a0a
邓宇 <313187515@qq.com>
2023-06-18 11:05:18 +0800

    设备列表渲染

--- src/renderer/components/Dashboard/Dashboard.tsx
+++ src/renderer/components/Dashboard/Dashboard.tsx
@@ -1,12 +1,28 @@
-import { Button, Space, Divider } from 'antd';
+import { Button, Space, Divider, Dropdown, MenuProps } from 'antd';
 import './Dashboard.scss';
+import { useState } from 'react';
 import adbHandlerManager from '../../core/adb/adbManager';
+import { DeviceInfo } from '../../core/types/device';
 
 export default function Dashboard() {
+  const [deviceList, setDeviceList] = useState<MenuProps['items']>([]);
+  const [currentDevice, setCurrentDevice] = useState<{
+    key: string;
+    label: string;
+  }>();
   const getDeviceList = async () => {
-    const deviceList = await adbHandlerManager.getDeviceList();
+    const devices: DeviceInfo[] = await adbHandlerManager.getDeviceList();
+    setDeviceList(
+      devices.map((deviceInfo) => {
+        return {
+          key: deviceInfo.serial,
+          label: `${deviceInfo.serial}   ${deviceInfo.state}`,
+        };
+      })
+    );
     console.log('deviceList', deviceList);
   };
+
   return (
     <div className="module">
       <h3>adb 命令</h3>
@@ -15,6 +31,14 @@ export default function Dashboard() {
         <Button onClick={getDeviceList} type="primary">
           设备列表
         </Button>
+        <Dropdown
+          menu={{
+            items: deviceList,
+          }}
+          placement="topLeft"
+        >
+          <Button>{currentDevice ? currentDevice.label : '选择设备'}</Button>
+        </Dropdown>
       </Space>
     </div>
   );
